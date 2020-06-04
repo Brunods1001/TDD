aa655e339b5dba673aa598d32ea42c363381c823
Brunods1001 <Brunods1001@gmail.com>
2020-06-03 22:12:45 -0400

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Safari()
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # James heard about a new online to-do app. He goes to checkout its
         # homepage.
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
@@ -42,6 +43,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly'
@@ -56,6 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # site has generated a unique URL - his to-do list is still there.
 
         # Satisfied, he goes to sleep.
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')

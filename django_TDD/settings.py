909827d6db332a42535afdc7d8255dcdbceeb91b
Brunods1001 <Brunods1001@gmail.com>
2020-06-07 12:38:51 -0400

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- django_TDD/settings.py
+++ django_TDD/settings.py
@@ -18,15 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '75z9g*ub!w^(3+n=itw^f*d14fbv33)7qyu4t6n97^)!r()epv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = '75z9g*ub!w^(3+n=itw^f*d14fbv33)7qyu4t6n97^)!r()epv'
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 

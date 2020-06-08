1576fe98bc40e4f933b779aa69a0ed24af9d4520
Brunods1001 <Brunods1001@gmail.com>
2020-06-08 15:26:55 -0400

    Moved Fts into their own files

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -1,43 +1,11 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import os
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-MAX_WAIT = 10
+from .base import FunctionalTest
 
-class NewVisitorTest(StaticLiveServerTestCase):
 
-    def setUp(self):
-        #self.browser = webdriver.Safari()
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # James heard about a new online to-do app. He goes to checkout its
@@ -127,26 +95,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy vegetables', page_text)
 
         # they are satisfied
-
-    def test_layout_and_styling(self):
-        # James goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # He notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        print('INPUTBOX LOCATION', inputbox.location['x'])
-        print('INPUTBOX WIDTH', inputbox.size['width'])
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512, delta=10)
-
-        # He starts a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512, delta=10
-        )

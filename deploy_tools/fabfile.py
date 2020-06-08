85f0f38b7acfc169035d241386374812c94b1f15
Brunods1001 <Brunods1001@gmail.com>
2020-06-08 11:36:29 -0400

    Updated all deploy_tool files using copy and paste to see if there was typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,20 +2,20 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = 'https://github.com/Brunods1001/TDD'
+REPO_URL = 'https://github.com/Brunods1001/TDD.git'
 
 def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
         run(f'git clone {REPO_URL} .')
-    current_commit = local('git log -n 1 --format=%H', capture=True)
+    current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f'git reset --hard {current_commit}')
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
-        run(f'python3 -m venv virtualenv')
-    run('./virtualenv/bin/pip3 install -r requirements.txt')
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
@@ -28,14 +28,13 @@ def _create_or_update_dotenv():
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
-    run('./virtualenv/bin/python3 manage.py collectstatic --noinput')
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 def _update_database():
-    run('./virtualenv/bin/python3 manage.py migrate --noinput')
+    run('./virtualenv/bin/python manage.py migrate --noinput')
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
-
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source()
@@ -43,3 +42,4 @@ def deploy():
         _create_or_update_dotenv()
         _update_static_files()
         _update_database()
+

bc47181d191b93e7ee3397866d5c1f8889d9cfe5
Brunods1001 <Brunods1001@gmail.com>
2020-06-15 09:52:03 -0400

    Added functional tests to validate list items with Javascript

---
+++ README.md
@@ -0,0 +1,1 @@
+#Making a To-Do list app to learn Test-Driven Development
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from django.utils.html import escape
 from unittest import skip
 
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import (
+    EMPTY_ITEM_ERROR, ItemForm,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm
+)
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -101,7 +104,6 @@ class ListViewTest(TestCase):
         expected_error = escape(EMPTY_ITEM_ERROR)
         self.assertContains(response, expected_error)
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1,
@@ -110,17 +112,21 @@ class ListViewTest(TestCase):
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'lists/list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
-
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response  = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'],
+                              ExistingListItemForm)
 
 class NewListTest(TestCase):
 

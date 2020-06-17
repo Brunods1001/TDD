f56707031c2fa53027c0ce3fdaf9ff7ef1e0a533
Brunods <Brunods1001@gmail.com>
2020-06-17 09:36:12 -0400

    Changed name of initialize() JQuery function. Used onload boilerplate to make sure DOM is loaded fully.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-var initialize = function() {
+window.Superlists = {}; // for a unique namespace
+window.Superlists.initialize = function() {
     $('input[name="text"]').on('keydown', function () {
         $('.has-error').hide();
     });
